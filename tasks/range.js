ba0978b2a13ebeb0b5ae93750764c21386804c53
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-04 16:40:32 +0300

    Tasks

---
+++ tasks/range.js
@@ -0,0 +1,28 @@
+// let range = 5;
+
+// -----------------------------------------------------
+// В диапазоне
+
+// if(range >= 14 && range <= 90) {
+//     console.log('in the range: range >= 14 && range <= 90');
+// }else{
+//     console.log('out of range');
+// }
+
+// (range >= 14 && range <= 90)
+//     ? console.log('in the range: range >= 14 && range <= 90')
+//     : console.log('out of range');
+
+// -----------------------------------------------------
+// Вне диапазона
+
+// if(range <= 14 || range >= 90) {
+//     console.log('out of range: range <= 14 || range >= 90');
+// }else{
+//     console.log('in the range');
+// }
+
+// (range <= 14 || range >= 90)
+//     ? console.log('out of range: range <= 14 || range >= 90')
+//     : console.log('in the range');
+
