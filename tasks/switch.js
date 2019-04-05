2749d3d3445ad79c9f69abfe5a6416afc9a75d0d
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-05 12:47:51 +0300

    Tasks

--- tasks/switch.js
+++ tasks/switch.js
@@ -31,15 +31,27 @@
 // --------------------------------------------------------------
 // Еще простой пример if else в switch
 
-let a = +prompt('a?', '');
+// let a = +prompt('a?', '');
 
-if (a == 0) {
-    console.log(0);
-}
-if (a == 1) {
-    console.log(1);
-} 
-if (a == 2 || a == 3) {
-    console.log('2,3');
-}
+// if (a == 0) {
+//     console.log(0);
+// }
+// if (a == 1) {
+//     console.log(1);
+// } 
+// if (a == 2 || a == 3) {
+//     console.log('2,3');
+// }
 
+// switch(a) {
+//     case 0:
+//         console.log(0);
+//         break;
+//     case 1:
+//         console.log(1);
+//         break;
+//     case 2:
+//     case 3:
+//         console.log('2,3');
+//         break;
+// }
