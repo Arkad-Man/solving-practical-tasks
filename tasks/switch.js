1b29ad238d9413407ad025d864fb6066241d2b2e
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-05 10:28:25 +0300

    Tasks

---
+++ tasks/switch.js
@@ -0,0 +1,45 @@
+// Простой switch переписаный в if else
+
+// let browser = '';
+
+// switch (browser) {
+//     case 'IE':
+//         console.log('У вас IE! :(');
+//         break;
+//     case 'Chrome':
+//     case 'Opera':
+//     case 'Safari':
+//     case 'Firefox':
+//         console.log('Эти браузеры мы поддерживаем!');
+//         break;
+//     default:
+//         console.log('Такого браузера мы не знаем!');
+// }
+
+// if(browser === 'IE') {
+//     console.log('У вас IE! :(');
+// }else if(browser === 'Chrome' 
+//         || browser === 'Opera' 
+//         || browser === 'Safari' 
+//         || browser === 'Firefox') {
+//     console.log('Эти браузеры мы поддерживаем!');
+// }else {
+//     console.log('Такого браузера мы не знаем!');
+// }
+
+
+// --------------------------------------------------------------
+// Еще простой пример if else в switch
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+    console.log(0);
+}
+if (a == 1) {
+    console.log(1);
+} 
+if (a == 2 || a == 3) {
+    console.log('2,3');
+}
+
