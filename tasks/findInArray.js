ae2ce08650065ec292b1d31a9ed83f65e932e800
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-07 20:10:26 +0300

    Tasks

---
+++ tasks/findInArray.js
@@ -0,0 +1,17 @@
+// Функция find(arr, value), которая ищет в массиве arr значение value
+// и возвращает его номер, если найдено, или -1, если не найдено.
+
+// let arr = ["test", 2, 1.5, false];
+
+// const find = (arr, val) => {
+//     for(let i = 0; i < arr.length; i++){
+//         if(val === arr[i]){
+//             return console.log(i);
+//         }
+//     }
+//     return console.log(-1);
+// }
+// find(arr, "test");
+// find(arr, 2);
+// find(arr, false);
+// find(arr, 5);
