aa24c66f02d19a07a0b5bb8f96659dfb5a26d909
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-08 13:57:06 +0300

    Tasks

---
+++ tasks/functionADD.js
@@ -0,0 +1,21 @@
+// Функция add(a, b) == add(a)(b).
+
+
+// const add = (a, b) => (a && b) ? a + b : (c) => a + c;
+
+// console.log(add(2, 5));
+// console.log(add(2)(5));
+
+// -------------------------------------------------------------
+
+// function add(a, b){
+//     if(a && b){
+//         return a + b;
+//     };
+//     return function(c){
+//         return a + c;
+//     };
+// }
+
+// console.log(add(2, 5));
+// console.log(add(2)(5))
