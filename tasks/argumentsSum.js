2f0b6a4afaf3c7919a071931f505b987cb941cd6
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-08 22:42:33 +0300

    Tasks

---
+++ tasks/argumentsSum.js
@@ -0,0 +1,19 @@
+// Функция sumArg() через метод reduce.
+
+// const sumArg = (...arg) => arg.reduce((a, b) => {return a + b}, 0);
+// console.log(sumArg(1, 3, 6, 20, 3));
+// console.log(sumArg());
+
+
+// --------------------------------------------------------------------
+// Функция sumArg() через цикл for().
+
+// function sumArg() {
+//     let res = 0;
+//     for(let i = 0; i < arguments.length; i++){
+//         res += arguments[i];
+//     }
+//     return res;
+// }
+// console.log(sumArg(1, 3, 6, 20, 3));
+// console.log(sumArg());
