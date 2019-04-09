a50c2d49c5128ecc4666ed969b677bc71c19160a
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-09 17:14:43 +0300

    Tasks

--- tasks/filterRange.js
+++ tasks/filterRange.js
@@ -1,4 +1,6 @@
-let array = [4, 7, 5, 1, 9, 0, 2, 8, 3];
+// Функция filterdRange() через метод forEach().
+
+// let array = [4, 7, 5, 1, 9, 0, 2, 8, 3];
 
 // let filterdRange = (arr, a, b) => {
 //     let array = [];
@@ -16,17 +18,43 @@ let array = [4, 7, 5, 1, 9, 0, 2, 8, 3];
 // console.log(filterdRange(array, -1, 10));
 
 
-let filterdRange = (arr, a, b) => {
-    let array = [];
-    for(let i = 0; i < arr.length; i++) {
-        if(a <= arr[i] && arr[i] <= b){
-            array.push(arr[i]);
-        }
-    };
-    return array;
-}
-console.log(filterdRange(array, 3, 8));
-console.log(filterdRange(array, 2, 8));
-console.log(filterdRange(array, 4, 6));
-console.log(filterdRange(array, -1, 7));
-console.log(filterdRange(array, -1, 10));
+// ------------------------------------------------------
+// Функция filterdRange() через цикл for().
+
+// let array = [4, 7, 5, 1, 9, 0, 2, 8, 3];
+
+// let filterdRange = (arr, a, b) => {
+//     let array = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         if(a <= arr[i] && arr[i] <= b){
+//             array.push(arr[i]);
+//         }
+//     };
+//     return array;
+// }
+// console.log(filterdRange(array, 3, 8));
+// console.log(filterdRange(array, 2, 8));
+// console.log(filterdRange(array, 4, 6));
+// console.log(filterdRange(array, -1, 7));
+// console.log(filterdRange(array, -1, 10));
+
+
+// ------------------------------------------------------
+// Функция filterdRange() через метод map().
+
+// let array = [4, 7, 5, 1, 9, 0, 2, 8, 3];
+
+// let filterdRange = (arr, a, b) => {
+//     let array = [];
+//     arr.map(elem => {
+//         if(a <= elem && elem <= b){
+//             array.push(elem);
+//         }
+//     });
+//     return array;
+// }
+// console.log(filterdRange(array, 3, 8));
+// console.log(filterdRange(array, 2, 8));
+// console.log(filterdRange(array, 4, 6));
+// console.log(filterdRange(array, -1, 7));
+// console.log(filterdRange(array, -1, 10));
