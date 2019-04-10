5be979260edda5f6e9b8a3bca7c3c4be3ed736b3
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-10 18:07:02 +0300

    Tasks

---
+++ tasks/counter.js
@@ -0,0 +1,15 @@
+//Функция makeCounter() - Счётчик.
+
+// const makeCounter = () => {
+//     let counter = 1;
+//     return () => counter++;
+// }
+
+// let counter1 = makeCounter();
+// console.log('counter1 = ' + counter1());
+// console.log('counter1 = ' + counter1());
+// console.log('counter1 = ' + counter1());
+
+// let counter2 = makeCounter();
+// console.log('counter2 = ' + counter2());
+// console.log('counter2 = ' + counter2());
