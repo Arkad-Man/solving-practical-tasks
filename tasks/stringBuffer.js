cef36e9419ae42d8b4c18c88aa9aa388ffd7c86e
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-10 20:13:24 +0300

    Tasks

---
+++ tasks/stringBuffer.js
@@ -0,0 +1,24 @@
+// Функция strBuffer().
+
+// function strBuffer() {
+//     let text = '';
+//     function buffer(val) {
+//         return (arguments.length !== 0) ? text += val : text;
+//     };
+//     buffer.clear = () => text = '';
+//     return buffer;
+// };
+
+// let buffer = strBuffer();
+
+// buffer('Надо ');
+// buffer('учить ');
+// buffer('Java Script!');
+// console.log(buffer());
+
+// buffer.clear();
+
+// buffer(1);
+// buffer(2);
+// buffer(3);
+// console.log(buffer());
