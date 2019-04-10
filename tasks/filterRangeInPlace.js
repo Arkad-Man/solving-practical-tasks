3bee1c07a048c95bfd5a41c761fe4831cd3e28da
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-10 15:39:59 +0300

    Tasks

--- tasks/filterRangeInPlace.js
+++ tasks/filterRangeInPlace.js
@@ -2,8 +2,7 @@
 // с числами arr и удаляет из него все числа вне диапазона a..b.
 
 // let array = [4, 7, 5, 1, 6, 9, 0, 2, 8, 3];
-
-// let filterdRangeInPlace = (arr, a, b) => {
+// let filterRangeInPlace = (arr, a, b) => {
 //     for(let i = 0; i < arr.length; i++){
 //         if(arr[i] < a || arr[i] > b){
 //             arr.splice(i--, 1);
@@ -11,38 +10,54 @@
 //     }
 // }
 
-// filterdRangeInPlace(array, 2, 6);
+// filterRangeInPlace(array, 2, 6);
 // console.log(array);
 
 
 // ------------------------------------------------------------------
-// Функция filterRangeInPlace() с использованием filter. (Работает не совсем корректно).
+// Функция filterRangeInPlace() с использованием filter().
 
 // let array = [4, 7, 5, 1, 6, 9, 0, 2, 8, 3];
-// let filterdRangeInPlace = (arr, a, b) => {
-//     arr.filter((elem, i) => {
-//         if(elem < a || elem > b){
-//             arr.splice(i--, 1);
-//         };
-//     });   
+// let filterRangeInPlace = (arr, a, b) => {
+//     let result = arr.filter((elem) => (elem >= a && elem <= b));
+//     return result;
 // }
 
-// filterdRangeInPlace(array, 2, 6);
+// array = filterRangeInPlace(array, 2, 6);
 // console.log(array);
 
 
 // -------------------------------------------------------------------
-// Функция filterRangeInPlace() с использованием forEach. (Работает не совсем корректно).
+// Функция filterRangeInPlace() с использованием forEach().
 
 // let array = [4, 7, 5, 1, 6, 9, 0, 2, 8, 3];
-
-// let filterdRangeInPlace = (arr, a, b) => {
-//     arr.forEach((elem, i) => {
-//         if(elem < a || elem > b){
-//             arr.splice(i--, 1);
-//         }
-//     })    
+// let filterRangeInPlace = (arr, a, b) => {
+//     let result = [];
+//     arr.forEach((elem) => {
+//         if(elem >= a && elem <= b){
+//             result.push(elem);
+//         };
+//     })
+//     return result;
 // }
 
-// filterdRangeInPlace(array, 2, 6);
+// array = filterRangeInPlace(array, 2, 6);
+// console.log(array);
+
+
+// -------------------------------------------------------------------
+// Функция filterRangeInPlace() с использованием map().
+
+// let array = [4, 7, 5, 1, 6, 9, 0, 2, 8, 3];
+// let filterRangeInPlace = (arr, a, b) => {
+//     let result = [];
+//     arr.map((elem) => {
+//         if(elem >= a && elem <= b){
+//             result.push(elem);
+//         };
+//     })
+//     return result;
+// }
+
+// array = filterRangeInPlace(array, 2, 6);
 // console.log(array);
