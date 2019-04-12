ff862b9d864ed1ed60709311a36bdf1b5f2c779a
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-12 11:23:00 +0300

    Tasks

---
+++ tasks/calculatorObj.js
@@ -0,0 +1,44 @@
+// Объект calculator.
+
+// const calculator = {
+//     read: function(){
+//       this.num1 = +prompt('Введите первое число!', 0);
+//       this.num2 = +prompt('Введите второе число!', 0);
+//     },
+//     sum: function(){
+//         return this.num1 + this.num2;
+//     },
+//     sub: function(){
+//         return this.num1 - this.num2;
+//     },
+//     mul: function(){
+//         return this.num1 * this.num2;
+//     },
+//     div: function(){
+//         return this.num1 / this.num2;
+//     }
+// };
+// calculator.read();
+// console.log(calculator.sum());
+// console.log(calculator.sub());
+// console.log(calculator.mul());
+// console.log(calculator.div());
+
+// ----------------------------------------------------------
+
+// const calculator = {
+//     read() {
+//       this.num1 = +prompt('Введите первое число!', 0);
+//       this.num2 = +prompt('Введите второе число!', 0);
+//     },
+//     sum() { return this.num1 + this.num2; },
+//     sub() { return this.num1 - this.num2; },
+//     mul() { return this.num1 * this.num2; },
+//     div() { return this.num1 / this.num2; }
+// };
+// calculator.read();
+// console.log(calculator.sum());
+// console.log(calculator.sub());
+// console.log(calculator.mul());
+// console.log(calculator.div());
+
