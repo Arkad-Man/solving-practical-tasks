d752c2075d8952c0fbfb8a6cf99d93b9acc22dbf
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-16 13:15:33 +0300

    Tasks

---
+++ tasks/concatenationArgument.js
@@ -0,0 +1,17 @@
+// function concatenation(arg) {
+//     for(let i = 1; i < arguments.length; i++){
+//         arguments[0] += arguments[i];
+//     }
+//     return arguments[0];
+// };
+
+// console.log(concatenation('*', '1', 'b', '1c'));
+
+// ---------------------------------------------------------------------------
+
+// const concatenation = (...arg) => {
+//     let res = arg.reduce((a, b) => a + b);
+//     return res;
+// };
+
+// console.log(concatenation('*', '1', 'b', '1c'));
