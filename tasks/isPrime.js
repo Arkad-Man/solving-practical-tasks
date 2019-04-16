7fb49d6b4cd21f7d8d4029e2f1ffc599965874b3
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-16 23:16:20 +0300

    Tasks

---
+++ tasks/isPrime.js
@@ -0,0 +1,22 @@
+// Функция isPrime().
+
+// function isPrime(num) {
+//     for(var i = 2; i < num; i++){
+//         if(num % i === 0) {
+//             return false;
+//         }else{
+//             return true;
+//         }
+//     }
+//     return num > 1;
+// }
+
+// console.log(isPrime(0));
+// console.log(isPrime(1));
+// console.log(isPrime(2));
+// console.log(isPrime(4));
+// console.log(isPrime(5));
+// console.log(isPrime(7));
+// console.log(isPrime(8));
+// console.log(isPrime(17));
+// console.log(isPrime(10000000000000));
