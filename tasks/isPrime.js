41d66beb0594bb88bdfd135a0dc094bfedabd5d8
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-17 19:31:13 +0300

    Tasks

--- tasks/isPrime.js
+++ tasks/isPrime.js
@@ -1,14 +1,15 @@
 // Функция isPrime().
 
 // function isPrime(num) {
+//     if(num <= 1) return false;
 //     for(var i = 2; i < num; i++){
+//         console.log('i = ' + i);
+//         console.log('num = ' + num);
 //         if(num % i === 0) {
 //             return false;
-//         }else{
-//             return true;
 //         }
 //     }
-//     return num > 1;
+//     return true;
 // }
 
 // console.log(isPrime(0));
@@ -18,5 +19,35 @@
 // console.log(isPrime(5));
 // console.log(isPrime(7));
 // console.log(isPrime(8));
+// console.log(isPrime(9));
 // console.log(isPrime(17));
+// console.log(isPrime(21));
+// console.log(isPrime(43));
+// console.log(isPrime(10000000000000));
+
+// ----------------------------------------------------------
+
+// function isPrime(num) {
+//     let prime = true;
+//     if(num <= 1) prime = false;
+//     for(var i = 2; i < num; i++){
+//         if(num % i === 0) {
+//             prime = !prime;
+//             break;
+//         }
+//     }
+//     return prime;
+// };
+
+// console.log(isPrime(0));
+// console.log(isPrime(1));
+// console.log(isPrime(2));
+// console.log(isPrime(4));
+// console.log(isPrime(5));
+// console.log(isPrime(7));
+// console.log(isPrime(8));
+// console.log(isPrime(9));
+// console.log(isPrime(17));
+// console.log(isPrime(21));
+// console.log(isPrime(43));
 // console.log(isPrime(10000000000000));
