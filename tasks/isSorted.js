3ffbc926aa023412b5cf4eb3b4a46a96a1673ebc
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-17 22:54:52 +0300

    Tasks

--- tasks/isSorted.js
+++ tasks/isSorted.js
@@ -1,1 +1,59 @@
+// Функция isSorted().
 
+// const isSorted = (mas) => {
+//     let item = mas[0];
+//     for(let i = 0; i < mas.length; i++){
+//         if(item > mas[i]){
+//             return false;
+//         }
+//         item = mas[i];
+//     }
+//     return true;
+// };
+
+// console.log(isSorted([]));
+// console.log(isSorted([-Infinity, -5, 0, 3, 9]));
+// console.log(isSorted([3, 9, -3, 10]));
+// console.log(isSorted([-4, 5, 1, 3, 9, 10]));
+
+// -------------------------------------------------------------
+
+// const isSorted = (mas) => {
+//     let item = mas[0];
+//     let res = true;
+//     mas.map((elem) => (item > elem) ? res = false : item = elem)
+//     return res;
+// };
+
+// console.log(isSorted([]));
+// console.log(isSorted([-Infinity, -5, 0, 3, 9]));
+// console.log(isSorted([3, 9, -3, 10]));
+// console.log(isSorted([-4, 5, 1, 3, 9, 10]));
+
+// -------------------------------------------------------------
+
+// const isSorted = (mas) => {
+//     let item = mas[0];
+//     let res = true;
+//     mas.forEach((elem) => (item > elem) ? res = false : item = elem)
+//     return res;
+// };
+
+// console.log(isSorted([]));
+// console.log(isSorted([-Infinity, -5, 0, 3, 9]));
+// console.log(isSorted([3, 9, -3, 10]));
+// console.log(isSorted([-4, 5, 1, 3, 9, 10]));
+
+// --------------------------------------------------------------
+
+// const isSorted = (mas) => {
+//     let item = mas[0];
+//     let res = true;
+//     mas.filter((elem) => (item > elem) ? res = false : item = elem)
+//     return res;
+// };
+
+// console.log(isSorted([]));
+// console.log(isSorted([-Infinity, -5, 0, 3, 9]));
+// console.log(isSorted([3, 9, -3, 10]));
+// console.log(isSorted([-4, 5, 1, 3, 9, 10]));
