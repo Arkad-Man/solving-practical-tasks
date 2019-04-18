b73aa532636dead635a90ea43a73149d9d1e6309
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-18 16:11:23 +0300

    Tasks

---
+++ tasks/myIndexOf.js
@@ -0,0 +1,41 @@
+// Функция myIndexOf().
+
+// const myIndexOf = (mas, value) => {
+//     let res = -1;
+//     mas.forEach((elem) => {
+//         if(elem === value) res = 0;
+//     })
+//     return res;
+// };
+
+// console.log(myIndexOf([1, 2, 3], 1));
+// console.log(myIndexOf([1, 2, 3], 4));
+// console.log(myIndexOf([5, 2, 0, 7, 9, -2, 13], 2));
+
+// -----------------------------------------------------------------
+
+// const myIndexOf = (mas, value) => {
+//     for(let i = 0; i < mas.length; i++){
+//         if(mas[i] === value) return 0;
+//     }
+//     return -1;
+// };
+
+// console.log(myIndexOf([1, 2, 3], 1));
+// console.log(myIndexOf([1, 2, 3], 4));
+// console.log(myIndexOf([5, 2, 0, 7, 9, -2, 13], 2));
+
+// -----------------------------------------------------------------
+
+// const myIndexOf = (mas, value) => {
+//     let res = -1;
+//     mas.filter((elem) => {
+//         if(elem === value) res = 0;
+//     })
+//     return res;
+// };
+
+// console.log(myIndexOf([1, 2, 3], 1));
+// console.log(myIndexOf([1, 2, 3], 4));
+// console.log(myIndexOf([5, 2, 0, 7, 9, -2, 13], 2));
+
