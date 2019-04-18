78f85de4a435482e30a412dffaf78935f06d5ed8
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-18 20:42:36 +0300

    Tasks

---
+++ tasks/missing.js
@@ -0,0 +1,15 @@
+// Функция missing().
+// const missing = (mas) => {
+//     let nMas = mas.slice(0).sort((a, b) => a - b);
+//     let miss = [];
+//     for (let i = 1; i <= nMas[nMas.length - 1]; i++) {
+// 	    if (nMas.indexOf(i) === -1) {
+// 		    miss.push(i);
+// 	    }
+//     }
+//     return miss;
+// }
+
+// console.log(missing([3, 1, 6, 2, 8, 11]));
+// console.log(missing([5, 1, 4, 2]));
+// console.log(missing([1, 2, 3, 4]));
