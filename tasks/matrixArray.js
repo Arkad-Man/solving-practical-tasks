c5c682a6af070078b339cb720138f590907f03a2
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-22 22:42:58 +0300

    Tasks

---
+++ tasks/matrixArray.js
@@ -0,0 +1,12 @@
+// Функция transformArr()
+
+// let arr1 = [[[1, 2]], [3]];
+// let arr2 = [[[1, 2]], [3], [[4, 5]], 6, [[[7]]], [[[[[[[[8]], 9]]]]]]];
+
+// const transformArr = (arr) => {
+//     let res = arr.join().split(',');
+//     return res.map(el => Number(el));
+// }
+
+// console.log(transformArr(arr1));
+// console.log(transformArr(arr2));
