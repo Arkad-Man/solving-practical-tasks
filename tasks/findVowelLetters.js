cb2e04f4a495cdf3e630f56593bf24d50275e37e
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-24 21:17:40 +0300

    Tasks

---
+++ tasks/findVowelLetters.js
@@ -0,0 +1,33 @@
+// Функция findVowelLetters().
+
+// const findVowelLetters = (str) => {
+//     const findVowelLetters = ['a', 'e', 'i', 'o', 'u', 'y'];
+//     let count = 0;
+//     for(let letter of str.toLowerCase()){
+//         if(findVowelLetters.indexOf(letter) !== -1){
+//             count += 1;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(findVowelLetters('Ozzy Osbourne'));
+// console.log(findVowelLetters('StarWars'));
+// console.log(findVowelLetters('WElcOme'));
+
+
+// const findVowelLetters = (str) => {
+//     const findVowelLetters = ['a', 'e', 'i', 'o', 'u', 'y'];
+//     let count = 0;
+//     let newStr = str.toLowerCase();
+//     for(let i = 0; i <= newStr.length; i++){
+//         if(findVowelLetters.indexOf(newStr[i]) !== -1){
+//             count += 1;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(findVowelLetters('Ozzy Osbourne'));
+// console.log(findVowelLetters('StarWars'));
+// console.log(findVowelLetters('WElcOme'));
