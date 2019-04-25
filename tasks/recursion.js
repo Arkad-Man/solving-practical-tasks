ffa401872f61e795318912e342e575f7d96edf86
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-04-25 22:44:56 +0300

    Tasks

---
+++ tasks/recursion.js
@@ -0,0 +1,47 @@
+// Recursion
+
+// const recurNum = (num) => {
+//     if(num === 1) {
+//         return 1;
+//     }
+//     return recurNum(num - 1) + ' ' + num;
+// };
+// console.log(recurNum(100));
+
+// ---------------------------------------------------------------------
+
+// const recurNum2 = (num1, num2) => {
+//     if(num1 < num2) return recurNum2(num1 + 1, num2) + ' ' + num1;
+    
+//     if(num1 === num2) return num1;
+
+//     if(num1 > num2) return recurNum2(num1 - 1, num2) + ' ' + num1;
+// };
+// console.log(recurNum2(90, 100));
+// console.log(recurNum2(9, 1));
+// console.log(recurNum2(90, 90));
+
+// ---------------------------------------------------------------------
+
+// const pow = (num, n) => (n !== 1) ? num * pow(num, n - 1) : num;
+
+// console.log(pow(2, 2));
+// console.log(pow(2, 10));
+
+// ---------------------------------------------------------------------
+
+// const sumTo = (num) => {
+//     if(num !== 1) return num + sumTo(num - 1);
+//     return 1
+// }
+
+// console.log(sumTo(20));
+// console.log(sumTo(200));
+
+// ---------------------------------------------------------------------
+
+// const factorial = (num) => (num !== 1) ? num * factorial(num - 1) : 1;
+
+// console.log(factorial(3));
+// console.log(factorial(10));
+
