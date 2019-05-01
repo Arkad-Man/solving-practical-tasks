d2ff730001c362538cb026ec5741caa3ee0b6705
arkadzi manyshau <man.arkad.nik@gmail.com>
2019-05-01 23:25:20 +0300

    Tasks

---
+++ tasks/binarySearch.js
@@ -0,0 +1,19 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const binarySearch = (arr, val) => {
+//   let min = 0;
+//   let max = arr.length - 1;
+//   let mid;
+//   while(min <= max){
+//     mid = Math.floor((min + max) / 2);
+//     if(val === arr[mid]) {
+//       return mid;
+//     }else if(val < arr[mid]) {
+//       max = mid - 1;
+//     }else{
+//       min = mid + 1;
+//     } 
+//   };
+//   return -1;
+// }
+// console.log(binarySearch(arr, 1))
